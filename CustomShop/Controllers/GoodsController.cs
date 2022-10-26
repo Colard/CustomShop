8ad8dee21058cf08aef572d70ebcfd54079882cb
danilka <kobernikdasa5@gmail.com>
2022-10-26 23:10:35 +0300

    Added starter design and user interface layot

---
+++ CustomShop/Controllers/GoodsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CustomShop.Controllers
+{
+    public class GoodsController : Controller
+    {
+        // GET: Goods
+        public ActionResult Index(string filter = null, int? min = null, int? max = null)
+        {
+            return View();
+        }
+    }
+}
